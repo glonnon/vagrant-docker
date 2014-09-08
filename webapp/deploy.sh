ac9e1f4fc0679ac54f928024b2edf3b2f74b7a8b
Greg Lonnon <greg.lonnon@gmail.com>
2014-09-08 07:13:53 -0600

    added gitignore

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,6 +1,10 @@
 echo "deploy"
 
+
+echo "make sure the DB and rabbitmq are up"
 /docker-wait $RABBITMQ_PORT_5672_TCP_ADDR $RABBITMQ_PORT_5672_TCP_PORT
 /docker-wait $MYSQL_PORT_3306_TCP_ADDR  $MYSQL_PORT_3306_TCP_PORT
 
+echo "check the health of the internets"
+/http-wait
 
