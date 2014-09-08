15d405f63bb1b5e3402bd7efc9e505743e1c7402
Greg Lonnon <greg.lonnon@gmail.com>
2014-09-08 08:03:44 -0600

    deploying etcd and waiting for it to come to ready...

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,6 +1,5 @@
 echo "deploy"
 
-
 echo "make sure the DB and rabbitmq are up"
 /docker-wait $RABBITMQ_PORT_5672_TCP_ADDR $RABBITMQ_PORT_5672_TCP_PORT
 /docker-wait $MYSQL_PORT_3306_TCP_ADDR  $MYSQL_PORT_3306_TCP_PORT
@@ -8,3 +7,5 @@ echo "make sure the DB and rabbitmq are up"
 echo "check the health of the internets"
 /http-wait
 
+echo "check for etcd to be up"
+/http-wait http://$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT/version
